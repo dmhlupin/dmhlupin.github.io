c14fc36b348caeeacd9df290b20b91361f8253fe
Dmitry Khlyupin <dmhlupin@gmail.com>
2021-03-19 13:34:25 +0300

    sample of component's methods and events work

--- vue_training/ex6/props/script.js
+++ vue_training/ex6/props/script.js
@@ -1,45 +1,43 @@
-Vue.component('v-body',{
-    props: ['items', 'quantity'],
+Vue.component('good-items',{
+    props:['items'],
     template: `
-        <div>
-            <p v-bind:quantity="quantity">Количество: {{quantity}}</p>
-            <button v-on:click="addQuantity">Add quantity</button>   
-            <div v-for="i in items">
-                <v-item v-bind:item='i'></v-item>
-            </div>
-        </div>        
-        `,
-    data: function(){
-        return {
-            itemQuantity: this.quantity
-        }
-    },
-    methods: {
-        addQuantity(){
-            this.$emit('quantity', this.itemQuantity +=1)
-        }
-    }
+    <div>
+    <good-item v-for="(item, index) in items"
+                    :item="item"
+                    :key="item.id"
+                    :index="index"
+                    v-on:add-item="$emit('add-item', $event)"
+                    v-on:remove="$emit('remove-item', $event)"
+    >
+    
+    </good-item>
+</div>
+    
+    `
 })
 
-Vue.component('v-item',{
-    props:['item'],
-    template: 
-        `
+
+
+Vue.component('good-item', {
+    props:['item', 'index'],
+    template: `
         <div>
-            <h3>{{item.name}}</h3>
-            <p>Price: {{item.price}}, Quantity: {{item.quantity}} </p>
-            <button class="addButton" v-on:click="$emit('add', item)">Add</button>
+            <p>Title: {{item.name}}</p>
+            <p>Price: {{item.price}}</p>
+            <p>Quantity: {{item.quantity}}</p>
+            <button v-on:click="add(index)">Add</button>
+            <button v-on:click="rem(index)">Remove</button>
         </div>
         `,
     methods: {
-        add(){
-            console.log('success');
-            this.$emit('add',this.item)
+        add(index){
+            this.$emit('add-item',index)
+        },
+        rem(index){
+            this.$emit('remove', index)
         }
     }
 })
-
-
 
 const app = new Vue({
     el: "#app",
@@ -49,14 +47,11 @@ const app = new Vue({
             {id: 1002, name: 'Монитор', price: 7400, quantity: 1},
             {id: 1003, name: 'Мышь', price: 600, quantity: 1},
             {id: 1004, name: 'Клавиатура', price: 1200, quantity: 1}
-        ],
-        quantity: 1
+        ]
     },
     methods: {
-        addItem(item){item += 1},
-        changeQuantity(value){
-            this.quantity = value;
-        }
+        addItem(index){this.items[index].quantity += 1},
+        removeItem(index){this.items[index].quantity -= 1}
     }
     
 })
