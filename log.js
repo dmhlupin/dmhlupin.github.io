36402c3cc3dd865dfd09eb8519ab11264c873cb0
Dmitry Khlyupin <dmhlupin@gmail.com>
2021-03-29 10:29:21 +0300

    HW7 complete

--- log.js
+++ log.js
@@ -1,10 +1,6 @@
 const fs = require('fs').promises;
 console.log('Запись статистики...')
-<<<<<<< HEAD
 async function log (type, id, name, client_ip) {
-=======
-async function log (type, id) {
->>>>>>> baf305861cbbbb17ce2fccba8e503c28e0e48759
     try{
         const data = await fs.readFile('./stats.json', 'utf-8');
         const logData = JSON.parse(data);
@@ -12,13 +8,9 @@ async function log (type, id) {
         const actionObj = {
             type,
             id,
-<<<<<<< HEAD
             name,
             time: (new Date()).toISOString(),
             client_ip
-=======
-            time: (new Date()).toISOString()
->>>>>>> baf305861cbbbb17ce2fccba8e503c28e0e48759
         };
         logData.push(actionObj);
         
